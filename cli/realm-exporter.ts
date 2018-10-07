f8d6d93a4eec5c2054ae0352e82aa52108cfed93
Diogo Osório <diogo.g.osorio@gmail.com>
2018-10-07 17:13:37 +0100

    Moves all the CLI commands to the realm-exporter.ts file
    As commander.js requires for all the CLI commands to be on the same
    directory, the package.json build caused the tool to fail (as it only
    copied the main command to the bin directory).
    
    This commit centralizes all the commands on a single file again.

--- cli/realm-exporter.ts
+++ cli/realm-exporter.ts
@@ -1,10 +1,76 @@
 #!/usr/bin/env node
 
+// tslint:disable:no-console
+
 import program from "commander";
-import { version } from '../package.json';
+import { version } from "../package.json";
+import * as fs from "fs";
+import * as exporter from "../src";
+import { csvFormatter } from "../src/formatters";
+
+program.version(version);
 
 program
-  .version(version)
-  .command('export <database> <object>', 'Exports the database object to a more common format')
-  .command('objects <database>', 'Lists all objects available on the database')
-  .parse(process.argv);
+  .command("export <database> <object>")
+  .description("Exports the database object to a more common format")
+  .action((database, objectType) => {
+    if (!database) {
+      console.error("The database argument is required");
+      process.exit(1);
+    }
+
+    if (!fs.existsSync(database)) {
+      console.error(`The database file ${database} doesn't exist`);
+      process.exit(1);
+    }
+
+    if (!objectType) {
+      console.error("The database object to be exported is required");
+      process.exit(1);
+    }
+
+    exporter
+      .load(database)
+      .then(exporter.select(objectType))
+      .then(csvFormatter)
+      .then(csvGenerator => {
+        for (const line of csvGenerator) {
+          console.log(line);
+        }
+
+        process.exit();
+      })
+      .catch(error => {
+        console.error(error.message);
+        process.exit(1);
+      });
+  });
+
+program
+  .command("objects <database>")
+  .description("Lists all objects available on the database")
+  .action(database => {
+    if (!database) {
+      console.error("The database argument is required");
+      process.exit(1);
+    }
+
+    if (!fs.existsSync(database)) {
+      console.error(`The database file ${database} doesn't exist`);
+      process.exit(1);
+    }
+
+    exporter
+      .load(database)
+      .then(exporter.getObjectNames)
+      .then(objects => {
+        objects.forEach(obj => console.log(obj));
+        process.exit();
+      })
+      .catch(error => {
+        console.error(error.message);
+        process.exit(1);
+      });
+  });
+
+program.parse(process.argv);
